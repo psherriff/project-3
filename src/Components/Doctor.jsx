f9080b6f2e114311f02ef357dbb66aaef8221e25
Pip Sherriff <pip.sherriff@gmail.com>
2023-05-11 19:43:27 -0500

    work with no errors thanks to viry

--- src/Components/Doctor.jsx
+++ src/Components/Doctor.jsx
@@ -5,16 +5,15 @@ const Doctor = (props) => {
         src={props.doctor.picture.headshot}
         alt={props.doctor.name.last}
         id="headshot"
-      >
-        <div id="info">
-          <h2>
-            {props.doctor.name.title}
-            {props.doctor.name.first} {props.doctor.name.last}
-          </h2>
-          <h3>{props.doctor.name.speciality - title}</h3>
-          <h3>{props.doctor.name.credentials}</h3>
-        </div>
-      </img>
+      />
+      <div id="info">
+        <h2>
+          {props.doctor.name.title}
+          {props.doctor.name.first} {props.doctor.name.last}
+        </h2>
+        <h3>{props.doctor.name.specialityTitle}</h3>
+        <h3>{props.doctor.name.credentials}</h3>
+      </div>
     </div>
   )
 }
