4f0a93b8ddcb4cdb4b57557dfb9cc4c2f2b8ce17
Pip Sherriff <pip.sherriff@gmail.com>
2023-05-18 18:14:14 -0500

    footer styling done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,15 +3,18 @@ const Footer = () => {
     <footer className="flex-ctr">
       <div className="container-footer">
         <div className="column-footer">
-          <img src="https://www.genesiscare.com/content/dam/asset-migration/icons,-logos/icons/GenesisCare_Logo_White_Desktop.svg" />
+          <img
+            src="https://www.genesiscare.com/content/dam/asset-migration/icons,-logos/icons/GenesisCare_Logo_White_Desktop.svg"
+            id="footer-logo"
+          />
         </div>
         <div className="column-footer">
-          <h3>GenesisCare</h3>
+          <h3>About us</h3>
           <h5>About</h5>
           <h5>Contact us</h5>
         </div>
         <div className="column-footer">
-          <h3>Website policies</h3>
+          <h3>Policies</h3>
           <h5>Privacy notice</h5>
           <h5>Cookies</h5>
         </div>
